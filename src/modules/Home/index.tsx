b477e7b7e7ff45b7b00fb3e395931ea2a4b05135
acm-97 <alejandro97.acm@gmail.com>
2023-01-01 17:08:51 -0500

    styles(home): update colors and transitions

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -12,16 +12,16 @@ const Home = () => {
       component="div"
       className="flex w-full flex-col items-start text-start"
       origin="bottom"
-      interval={300}
+      interval={200}
       easing="ease-in"
     >
       <h6 className="sec-color font--mono mb-5 ml-1 font-normal">{t('salute')}</h6>
 
-      <h1 className="mb-1 font-black">{t('name')}</h1>
-      <h1 className="font-black opacity-70">
+      <h1 className="primary-color-light mb-1 font-black">{t('name')}</h1>
+      <h1 className="primary-color-dark font-black">
         {t('occupation.part1')} <span className="sec-color">UI</span> {t('occupation.part2')}.
       </h1>
-      <p className="mt-5 max-w-xl text-xl opacity-70">{t('description')}</p>
+      <p className="primary-color-dark mt-5 max-w-xl text-xl">{t('description')}</p>
       <button type="button" className="btn-teal font--mono btn-outline btn mt-12">
         {t('downloadResume')}
       </button>
