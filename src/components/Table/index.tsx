481c3a3e87c0f68b67aad1e31193f2ac0b6db838
acm-97 <alejandro97.acm@gmail.com>
2023-01-03 01:44:01 -0500

    feat: add table component

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from 'uuid';
+import { memo } from 'react';
+import classnames from 'classnames';
+import ScrollReveal from '../ScrollReveal';
+
+export type ColumnsProps = {
+  id?: string;
+  accessor: any;
+  width?: number;
+  headerName?: string;
+  cellProps?: object;
+  headerCellProps?: object;
+};
+
+type TableProps = {
+  columns: ColumnsProps[];
+  rows: any[];
+  containerClass?: string;
+  tableClass?: string;
+  tableRowBodyProps?: any;
+  tableHeaderProps?: any;
+  scrollReveal?: boolean;
+};
+
+const Table = ({
+  columns,
+  rows,
+  containerClass,
+  tableClass,
+  tableRowBodyProps,
+  tableHeaderProps,
+  scrollReveal = false,
+}: TableProps) => (
+  <div className={classnames('overflow-x-auto', containerClass)}>
+    <table className={classnames('table w-full', tableClass)}>
+      <ScrollReveal component="thead" delay={!scrollReveal ? 0 : 500}>
+        <tr>
+          {columns.map((col) => (
+            <th key={uuidv4()} {...col.headerCellProps}>
+              {col.headerName}
+            </th>
+          ))}
+        </tr>
+      </ScrollReveal>
+      <ScrollReveal component="tbody" delay={!scrollReveal ? 0 : 500}>
+        {rows.map((row) => (
+          <tr key={uuidv4()} {...tableRowBodyProps} onClick={() => tableRowBodyProps?.onClick(row)}>
+            {columns.map((col) => (
+              <td key={uuidv4()} {...col.cellProps}>
+                {typeof col.accessor === 'function' ? col.accessor(row) : row[col.accessor]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </ScrollReveal>
+    </table>
+  </div>
+);
+
+export default memo(Table);
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,5 +5,6 @@ export { default as ScrollReveal } from './ScrollReveal';
 export { default as Button } from './Button';
 export { default as SiteLogo } from './Logo';
 export { default as Layout } from './MainLayout';
+export { default as Table } from './Table';
 
 export type { LinkProps } from './Link';
