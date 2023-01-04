745d436c8b05655cc2191a81c7e2292aa87dd4b1
acm-97 <alejandro97.acm@gmail.com>
2023-01-04 00:26:12 -0500

    refactor (logo loader)

--- src/components/IconComponents/loader.tsx
+++ src/components/IconComponents/loader.tsx
@@ -1,7 +1,7 @@
 // eslint-disable
 // @ts-nocheck
 import React, { useEffect } from 'react';
-import { ReactComponent as Svg } from '@/assets/icons/logo-loader.svg';
+import { ReactComponent as Svg } from '/public/logo-loader.svg';
 
 const IconLoader = (props: any) => {
   useEffect(() => {
