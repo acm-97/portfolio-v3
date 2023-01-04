c640fc8030a2b95f38b1a48e585c7a1e9b1b45e2
acm-97 <alejandro97.acm@gmail.com>
2023-01-03 23:37:26 -0500

    fix(build) rename ./Icon

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 export { default as SectionHeader } from './SectionHeader';
 export { default as Link } from './Link';
-export { default as Icon } from './Icons/IconRender';
-export { default as LoaderIcon } from './Icons/loader';
+export { default as Icon } from './IconComponents/IconRender';
+export { default as LoaderIcon } from './IconComponents/loader';
 export { default as ScrollReveal } from './ScrollReveal';
 export { default as Button } from './Button';
 export { default as SiteLogo } from './Logo';
